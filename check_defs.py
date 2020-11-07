e24cf75781f53cc242a8f0eabdcae11285f8cb77
Kostas Vlachos <kvlachos.geo@gmail.com>
2020-11-07 13:23:00 +0200

    Add check_defs.py
    This is a file that contains the useful functions to use for the operating system and hardware usage etc checks

---
+++ check_defs.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python3
+
+### Imports
+import psutil
+import pandas as pd
+from matplotlib import pyplot as plt
+import datetime as dt
+
+
+
+
+if __name__ == '__main__':
+	print('This is main')

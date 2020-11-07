0ebfb6a4f27362f5f39eb9c79b2d89c61ae5ec90
Kostas Vlachos <kvlachos.geo@gmail.com>
2020-11-07 14:56:43 +0200

    Add cpu_sampling function

--- check_defs.py
+++ check_defs.py
@@ -7,8 +7,32 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import datetime as dt
 
-
-
+def cpu_sampling(n_secs=0.1, sampling_duration=5):
+	"""
+	Arguments
+		n_secs -- interval between samplings in seconds
+		sampling_duration -- duration of sampling in seconds
+	Returns
+		cpu_lst -- list of cpu usage in percentage
+		cpu_time -- list of time of sampling in datetime object
+	"""
+	
+	# Initialization
+	cpu_lst = []
+	cpu_time = []
+	delta_t = dt.timedelta(seconds=sampling_duration) # duration of sampling
+	t_now = dt.datetime.now()
+	t_start = t_now # starting point
+	
+	while t_now < (t_start + delta_t):
+		cpu_lst.append(psutil.cpu_percent(n_secs))
+		cpu_time.append(t_now)
+		t_now = dt.datetime.now()
+	
+	return cpu_lst, cpu_time
+	
 
 if __name__ == '__main__':
-	print('This is main')
+
+	cpu_lst, cpu_time = cpu_sampling(n_secs=0.1)
+	print(len(cpu_lst))
